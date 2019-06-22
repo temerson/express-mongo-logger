6433d30004bb25214aeca4601c86fb1653fd4613
Tristan Emerson <tristan.emerson@echoman.com>
2019-06-21 22:32:57 -0400

    remove uuid

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,10 @@
 import { Router } from 'express';
-import uuidv4 from 'uuid/v4';
 import models from '../models';
 
 const router = Router();
 
 router.post('/', async (req, res) => {
-  const payload = await models.RouteChange.create({
-    id: uuidv4(),
-    ...req.body,
-  });
+  const payload = await models.RouteChange.create(req.body);
 
   return res.send(payload);
 });
